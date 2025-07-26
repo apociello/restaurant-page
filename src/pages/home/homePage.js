bb263a7852b3ad5c5377188892cf564aedd81b7b
pociello <asierpociello@gmail.com>
2025-07-27 00:17:56 +0200

    Redesign de base html and css - change the bg image - style the header: padding, font-size, postion,...

--- src/pages/home/homePage.js
+++ src/pages/home/homePage.js
@@ -1,14 +1,11 @@
-import "./home.css"
+/* import "./home.css"
 
 function homePage() {
-    const title = document.createElement('h1');
-    title.textContent = 'Quiet Cup';
-
     const intro = document.createElement('p');
     intro.textContent = 'A refined space where exceptional coffee meets a tranquil atmosphere. Ideal for focused work, thoughtful reading, or peaceful relaxation.'
 
     const content = document.getElementById('content');
-    content.append(title, intro)
+    content.append(intro)
 }
 
-export {homePage}
+export {homePage} */
