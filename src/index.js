47a46e9bdcc90dbff65903cb8a1db91d89b7d773
pociello <asierpociello@gmail.com>
2025-07-29 00:09:38 +0200

    Add eventlisteners to the nav buttons - when clicking Home the homePage js and css load in the .content div - Menu & contact btns just reset .content for now

--- src/index.js
+++ src/index.js
@@ -2,4 +2,21 @@ import "./style.css"
 import {homePage} from "./pages/home/homePage.js"
 console.log('Hello World!')
 
-homePage()
+const contentDiv = document.getElementById('content');
+
+const homeBtn = document.getElementById('home');
+const menuBtn = document.getElementById('menu');
+const contactBtn = document.getElementById('contact');
+
+homeBtn.addEventListener('click', () => {
+    contentDiv.textContent = '';
+    homePage()
+})
+
+menuBtn.addEventListener('click', () => {
+    contentDiv.textContent = '';
+})
+
+contactBtn.addEventListener('click', () => {
+    contentDiv.textContent = '';
+})
