aff1f2fe22c3da38c89e3eb9ef3c40e7a1fb559b
pociello <asierpociello@gmail.com>
2025-08-02 01:33:24 +0200

    Add snow border bottom to the selected tab

--- src/index.js
+++ src/index.js
@@ -9,18 +9,31 @@ const menuBtn = document.getElementById('menu');
 const contactBtn = document.getElementById('contact');
 
 homeBtn.addEventListener('click', () => {
+    homeBtn.style.borderBottom = '2px solid snow';
+    menuBtn.style.borderBottom = 'none';
+    contactBtn.style.borderBottom = 'none';
+    
     contentDiv.textContent = '';
     homePage()
 })
 
 menuBtn.addEventListener('click', () => {
+    homeBtn.style.borderBottom = 'none';
+    menuBtn.style.borderBottom = '2px solid snow';
+    contactBtn.style.borderBottom = 'none';
+
     contentDiv.textContent = '';
     menuPage()
 })
 
 contactBtn.addEventListener('click', () => {
+    homeBtn.style.borderBottom = 'none';
+    menuBtn.style.borderBottom = 'none';
+    contactBtn.style.borderBottom = '2px solid snow';
+
     contentDiv.textContent = '';
     contactPage()
 })
 
 homePage()
+homeBtn.style.borderBottom = "2px solid snow"
