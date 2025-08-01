252c2ab610684aa7ed02fbdbb4ca5f58b556e9ae
pociello <asierpociello@gmail.com>
2025-08-02 00:44:28 +0200

    Decorate the contact page with emojis

--- src/pages/contact/contactPage.js
+++ src/pages/contact/contactPage.js
@@ -5,25 +5,20 @@ function contactPage(){
     const contactDiv = document.createElement('div');
     contactDiv.classList.add('contact-div')
 
-    const address1 = document.createElement('p');
-    address1.textContent = '257 Hamilton Avenue'
-    const address2 = document.createElement('p');
-    address2.textContent = 'Palo Alto, CA 94301';
+    const address = document.createElement('p');
+    address.textContent = '📍 257 Hamilton Avenue, Palo Alto, CA 94301'
 
     const img = document.createElement('img');
     img.src = image;
     img.classList.add('image')
 
-
     const phoneNumber = document.createElement('p');
-    phoneNumber.textContent = '+1 (650) 555-1234';
+    phoneNumber.textContent = '📞 +1 (650) 555-1234';
 
     const email = document.createElement('p');
-    email.textContent = 'hello@quietcup.com';
+    email.textContent = '📧 hello@quietcup.com';
     
-    
-
-    contactDiv.append(address1, address2, img, phoneNumber, email)
+    contactDiv.append(address, img, phoneNumber, email)
     const content = document.getElementById('content');
     content.append(contactDiv)
 }
